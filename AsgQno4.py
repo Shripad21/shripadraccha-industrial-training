9f2618f3c1fe2c2e57edf06091ffa7ef9cea4878
Shripad21 <88705649+Shripad21@users.noreply.github.com>
2021-08-30 09:03:39 +0530

    Delete AsgQno4.py

--- AsgQno4.py
+++
@@ -1,5 +0,0 @@
-#importing package
-from fractions import Fraction
-percent=30 
-#displaying percentage in fraction using fraction function
-print("The numerator/denominator form of 30% is : ",Fraction(30,100))
